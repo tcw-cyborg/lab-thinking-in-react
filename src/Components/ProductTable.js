09ecc0d6d86038131e85bc8537d5902b724247dd
tcw-cyborg <tcwong007@gmail.com>
2021-07-03 16:27:22 +0200

    iteration 1 done

---
+++ src/Components/ProductTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductRow from './ProductRow';
+
+const ProductTable = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductTable;
